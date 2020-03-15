31797d6225cd409da1239ab539861e3935d3119d
Olena Dyskant <olena.dyskant@gft.com>
2020-03-15 01:19:06 +0100

    Checking brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function checkBrackets(txt) {
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      var pairedBr = bracketsConfig[i][0] + bracketsConfig[i][1];
+      if (txt.includes(pairedBr))
+        return checkBrackets(txt.split(pairedBr).join(''))
+    }
+    return txt;
+  }
+  return checkBrackets(str).length === 0;
 }
